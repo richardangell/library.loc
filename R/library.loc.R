e6e83f521aaa4db0f3dff8ff712703de4b96bfa6
richardangell <richardangell37@gmail.com>
2017-07-29 16:34:52 +0100

    in progress apporach to change .libPaths

--- R/library.loc.R
+++ R/library.loc.R
@@ -14,51 +14,44 @@
 #' library.loc(gbm, )
 #'
 #' @export
-library.loc <- function(package, loc) {
+library.loc <- function(package, loc, ...) {
+  
+  if (!dir.exists(loc)) {
+    
+    stop(gettextf("loc (%s) does not exist.", sQuote(loc)))
+    
+  }
   
   # quote package if it is not passed quoted already
   package <- as.character(substitute(package))
   
-  installed_packages <- data.frame(installed.packages(lib = loc),
-                                   stringsAsFactors = FALSE)
+  # record current libPaths
+  prev_libPaths <- .libPaths()
   
-  session_info <- sessionInfo()
+  print(gettextf(".libPaths() currently set to %s.", sQuote(prev_libPaths)))
   
-  #-----------------------------------------------------------------------------#
-  # Section 1. Check if package in installed in loc ----
-  #-----------------------------------------------------------------------------#
-  
-  pkg_installed <- package_in_dir(pkg = package,
-                                  dir = loc,
-                                  version = NULL)
-  
-  pkg_info <- installed_packages[installed_packages$Package == package, ]
-  
-  #-----------------------------------------------------------------------------#
-  # Section 2. Check if depends for package are installed in loc ----
-  #-----------------------------------------------------------------------------#
-  
-  if (!is.na(pkg_info$Depends)) {
+  if (prev_libPaths == loc) {
     
-    check_pkg_dependencies(pkg_info$Depends)
+    print("not reassigning .libPaths as it is already equal to loc.")
+    
+  } else {
+    
+    print("reassigning .libPaths()")
+    
+    assign(".lib.loc", loc, envir = environment(.libPaths))
+    
+    post_libPaths <- .libPaths()
+    
+    print(gettextf(".libPaths() set to %s.", sQuote(post_libPaths)))
+    
+    
     
   }
   
-  #-----------------------------------------------------------------------------#
-  # Section 3. Check if imports for package are installed in loc ----
-  #-----------------------------------------------------------------------------#
   
-  if (!is.na(pkg_info$Imports)) {
-    
-    check_pkg_dependencies(pkg_info$Imports)
-    
-  }
   
-  #-----------------------------------------------------------------------------#
-  # Section 4. Load package ----
-  #-----------------------------------------------------------------------------#
-  
-  library(package, loc = loc)
+
+  library(package, lib.loc = loc, ...)
   
 }
 
@@ -72,39 +65,5 @@ library.loc <- function(package, loc) {
 
 
 
-
-
-
-
-
-# split out each dependancy
-pkg_depends <- strsplit(pkg_info$Depends, ",")[[1]]
-
-pkg_depends <- trimws(pkg_depends, which = "both")
-
-for (i in 1:length(pkg_depends)) {
-  
-  # skip over dependancy if it is R
-  if (pkg_to_check_split[1] != "R") {
-    
-    is_package_in_loc(pkg_to_check[1], loc)
-    
-    depends_info <- installed_packages[installed_packages$Package == pkg_to_check_split[1], ]
-    
-    # if there is a version number specified for the dependancy
-    if (length(pkg_to_check_split) > 1) {
-      
-      # check that the version of the depend package is sufficient
-      pkg_version_check()
-      
-      
-      
-    }
-    
-  }
-  
-}
-
-}
 
 
